dd0f29330e27ed8a22102e79b394f5a8dbaba083
Troy Karan Harrison <NoxPardalis@protonmail.com>
2019-03-21 17:04:40 +0100

    Update backend routes to respond to both JSON and CSV

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -7,14 +7,14 @@ use rocket_contrib::json::Json;
 use rocket_contrib_local::csv::Csv;
 
 /// Get the JSON representation of the airports in the data store.
-#[get("/airports", format = "application/json")]
+#[get("/airports", format = "application/json", rank = 1)]
 fn json_route(data_store: State<DataStore>) -> Json<Vec<Airport>> {
     Json(data_store.airports().collect())
 }
 
 /// Get the CSV representation of the airports in the data store.
 #[get("/airports", format = "text/csv", rank = 2)]
-fn csv_route(data_store: State<DataStore>) -> String {
+fn csv_route(data_store: State<DataStore>) -> Csv<Vec<Airport>> {
     fn convertor(airports: &Vec<Airport>) -> String {
         let mut wtr = csv::Writer::from_writer(vec![]);
         for airport in airports {
@@ -22,10 +22,16 @@ fn csv_route(data_store: State<DataStore>) -> String {
         }
         String::from_utf8(wtr.into_inner().unwrap()).unwrap()
     };
-    Csv::new(data_store.airports().collect(), convertor).convert()
+    Csv::new(data_store.airports().collect(), convertor)
+}
+
+/// Get default representation of the airports in the data store if the format is not specified.
+#[get("/airports", rank = 3)]
+fn default_route(data_store: State<DataStore>) -> Json<Vec<Airport>> {
+    json_route(data_store)
 }
 
 /// Mount the routes to a moved `rocket` and return the resulting `rocket`.
 pub fn mount(rocket: Rocket) -> Rocket {
-    rocket.mount("/", routes!(json_route, csv_route))
+    rocket.mount("/", routes!(json_route, csv_route, default_route))
 }
