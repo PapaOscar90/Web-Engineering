2e8d41ff7dd002ff3d638a74bab3503068ad2ed5
Troy Karan Harrison <NoxPardalis@protonmail.com>
2019-03-28 12:49:13 +0100

    Remove backend for migration to cockroach db backed version

--- backend/rocket_contrib_local/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![deny(missing_docs)]
-
-//! This crate contains locally developed libraries that provide functionality
-//! used by Rocket applications not already provided by
-//! [rocket_contrib](https://api.rocket.rs/rocket_contrib/).
-
-pub mod csv;
